9f2bba4ddb855f00052a20fffba1bd00bc7b3869
Nikola Maljenovic <32510744+NikolaMC@users.noreply.github.com>
2021-09-16 12:07:03 +0200

    Testing

--- src/main/java/CarFactory.java
+++ src/main/java/CarFactory.java
@@ -1,16 +1,38 @@
 package main.java;
 
 public class CarFactory {
-    public Car getCar(String brand) throws CarException {
-        switch(brand) {
+
+    private String[] brands = {"Volvo", "Opel"};
+    private String[] engines = {"Engine1", "Engine2"};
+
+    private String getAlphaNumericString() {
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789";
+        StringBuilder sb = new StringBuilder(6);
+
+        for (int i = 0; i < 6; i++) {
+            int index = (int)(AlphaNumericString.length() * Math.random());
+            sb.append(AlphaNumericString.charAt(index));
+        }
+
+        return sb.toString();
+    }
+
+    public Car getCar(String color) throws CarException {
+        switch(brands[0]) {
             case "Opel": {
-                Opel opel = new Opel("Red");
-                System.out.println(opel.getRegistrationNumber());
+                Car opel = new Car(color);
+                opel.setRegNumber(getAlphaNumericString());
+                System.out.println(opel.getRegNumber());
+                opel.setBrand(brands[0]);
+                opel.setEngine(engines[0]);
                 return opel;
             }
             case "Volvo": {
-                Volvo volvo = new Volvo("White");
-                System.out.println(volvo.getRegistrationNumber());
+                Car volvo = new Car(color);
+                volvo.setRegNumber(getAlphaNumericString());
+                System.out.println(volvo.getRegNumber());
+                volvo.setBrand(brands[0]);
+                volvo.setEngine(engines[0]);
                 return volvo;
             }
             default: {
